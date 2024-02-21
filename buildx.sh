d51aa801fbe412b25e16b1f4a78884a8c5e1fede
Slava Kalashnikov <xemuliam@users.noreply.github.com>
2024-02-21 09:40:49 +0200

    Add files via upload

---
+++ buildx.sh
@@ -0,0 +1,30 @@
+CO_VER=1.7.8
+RS_VER=1.7.3
+SF_VER=1.7.2
+BQ_VER=1.7.5
+
+docker buildx create --use --name mybuilder && \
+docker run --pull always --privileged --rm tonistiigi/binfmt --install all
+
+# Redshift
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t xemuliam/dbt:${CO_VER}-redshift${RS_VER} \
+  -t xemuliam/dbt:$(echo $CO_VER | cut -d. -f 1,2)-redshift \
+  -t xemuliam/dbt:redshift \
+  --build-arg PLUGINS='core=='${CO_VER}',redshift=='${RS_VER} --push - < ~/Projects/Docker/dbt/Dockerfile.multistage.universal
+
+# Snowflake\
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t xemuliam/dbt:${CO_VER}-snowflake${SF_VER} \
+  -t xemuliam/dbt:$(echo $CO_VER | cut -d. -f 1,2)-snowflake \
+  -t xemuliam/dbt:snowflake \
+  --build-arg PLUGINS='core=='${CO_VER}',snowflake=='${SF_VER} --push - < ~/Projects/Docker/dbt/Dockerfile.multistage.universal
+
+# BigQuery\
+docker buildx build --platform linux/amd64,linux/arm64 \
+  -t xemuliam/dbt:${CO_VER}-bigquery${BQ_VER} \
+  -t xemuliam/dbt:$(echo $CO_VER | cut -d. -f 1,2)-bigquery \
+  -t xemuliam/dbt:bigquery \
+  --build-arg PLUGINS='core=='${CO_VER}',bigquery=='${BQ_VER} --push - < ~/Projects/Docker/dbt/Dockerfile.multistage.universal
+
+docker buildx rm -f && docker buildx prune -f
